305529a074cbac38ef9bf68c69f6f04b3a538c56
togawamanabu <togawamanabu@gmail.com>
2017-09-15 10:05:56 +0900

    lesson 2-5 combine redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
 export function addRecipe({ day, recipe, meal}) {
   return {
     type: ADD_RECIPE,
-    receipe,
+    recipe,
     day,
     meal,
   }
